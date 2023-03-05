6748a9ad82466fb15807589a34a8adc624991e3b
HuyNguyenAu <huynguyenau@gmail.com>
2023-03-05 21:50:20 +1100

    Added Github pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
 import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
-  integrations: [svelte(), tailwind()]
+  site: 'https://huynguyen.space',
+  integrations: [tailwind()]
 });
