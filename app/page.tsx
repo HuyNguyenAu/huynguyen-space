6138b71d285f6a75570f88de4138636b7f94d265
HuyNguyenAu <github@huynguyen.email>
2023-10-29 20:45:12 +1100

    WIP.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
+import Image from "next/image";
 import { PropsWithChildren } from "react";
+import HeroImage from "./hero.png";
 
 const NavigationLink = ({ text, href }: { text: string, href: string }) => {
   return (
@@ -72,8 +74,8 @@ const Leading = () => {
     <div className="h-full w-fit flex items-center">
       <div className="w-fit p-8 flex flex-col gap-y-12">
         <p className="p-1 w-fit text-center text-xs text-[#F9DEC9] font-bold tracking-[0.75em]">FULLSTACK SOFTWARE ENGINEER</p>
-        <p className="w-fit text-8xl text-[#F9DEC9] font-bold tracking-tight leading-[80%]">HUY<br />NGUYEN</p>
-        <p className="p-1 w-fit text-xs text-[#F9DEC9] tracking-tight">I'm a curious individual and an avid learner.<br />I thrive on tackling interesting and difficult challenges.</p>
+        <p className="w-fit text-8xl text-[#F9DEC9] font-bold tracking-wide leading-[85%]">HUY<br />NGUYEN</p>
+        <p className="p-1 w-fit text-xs text-[#F9DEC9]">I'm a curious individual and an avid learner.<br />I thrive on tackling interesting and difficult challenges.</p>
       </div>
     </div>
   );
@@ -83,10 +85,14 @@ const Page = () => {
   return (
     <div className="min-h-screen min-w-screen py-16 px-64 flex flex-col gap-y-8">
       <Navigation />
-      <div className="grow h-full w-full flex items-center">
+      <div className="grow h-full w-full flex justify-between items-center">
         <Leading />
-        <div className="border grow h-full w-fit flex items-center">
-        </div>
+        <Image
+          className="w-1/2 rounded-lg flex items-center"
+          src={HeroImage}
+          placeholder="blur"
+          alt=""
+        />
       </div>
     </div>
   );
