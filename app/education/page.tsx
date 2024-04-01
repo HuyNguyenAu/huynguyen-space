09006d986d45dfd9ed43e79326db79470f123aea
HuyNguyenAu <github@huynguyen.email>
2024-04-01 10:53:09 +0000

    Clean up.

--- app/education/page.tsx
+++ app/education/page.tsx
@@ -2,7 +2,8 @@ const Page = () => {
   return (
     <article className="prose dark:prose-invert text-[#F9DEC9] w-full">
       <h3>Computer and Network Engineering (Honours)</h3>
-      <p><small>Mar 2016 - Mar 2020</small><br /><small>RMIT</small></p>
+      <p><small>Mar 2016 - Mar 2020</small></p>
+      <p>RMIT</p>
     </article>
   );
 };
