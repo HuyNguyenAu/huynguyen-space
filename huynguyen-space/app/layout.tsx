091f8135ea23e8e06aba9fbd5ab294b1c7b5e294
Huy Nguyen <github@huynguyen.email>
2024-05-27 14:20:58 +1000

    Feature: Optimise Image (#17)

--- huynguyen-space/app/layout.tsx
+++ huynguyen-space/app/layout.tsx
@@ -1,4 +1,5 @@
-import type { Metadata } from 'next'
+import type { Metadata } from 'next';
+import Head from 'next/head';
 
 import { Inter } from 'next/font/google'
 import React from "react";
@@ -20,6 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/icon?<generated>" type="image/png" sizes="32x32" />
+      </Head>
       <body className={`bg-[#1B2432] ${inter.className}`}>
         <div className="flex min-h-screen flex-col items-center justify-center gap-y-32 p-8 md:px-16 lg:px-32 xl:px-64">
           <Navigation />
--- huynguyen-space/app/page.tsx
+++ huynguyen-space/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import React from "react";
 
-import HeroImage from "./hero.png";
+import HeroImage from "./hero_small.png";
 
 const Page = () => {
   return (
