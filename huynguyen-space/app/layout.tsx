26eb31848fa1a6daeaf9c1b5e0f087dd991c6068
HuyNguyenAu <github@huynguyen.email>
2024-05-27 14:10:14 +1000

    Fixed.

--- huynguyen-space/app/layout.tsx
+++ huynguyen-space/app/layout.tsx
@@ -1,4 +1,5 @@
-import type { Metadata } from 'next'
+import type { Metadata } from 'next';
+import Head from 'next/head';
 
 import { Inter } from 'next/font/google'
 import React from "react";
@@ -11,6 +12,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   description: 'Huy Nguyen - Personal Portfolio',
   title: 'Huy Nguyen - Personal Portfolio',
+  icons: 
 }
 
 export default function RootLayout({
@@ -20,9 +22,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
+      <Head>
         <link rel="icon" href="/icon?<generated>" type="image/png" sizes="32x32" />
-      </head>
+      </Head>
       <body className={`bg-[#1B2432] ${inter.className}`}>
         <div className="flex min-h-screen flex-col items-center justify-center gap-y-32 p-8 md:px-16 lg:px-32 xl:px-64">
           <Navigation />
