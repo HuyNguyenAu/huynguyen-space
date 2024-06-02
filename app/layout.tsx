609700b3474e35d59cfd6292e02c2b34e66a41e7
Huy Nguyen <github@huynguyen.email>
2024-06-02 20:19:09 +1000

    Feature: Fixed Errors (#20)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import Head from 'next/head';
 
 import { Inter } from 'next/font/google'
 import React from "react";
@@ -21,9 +20,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="/icon?<generated>" type="image/png" sizes="32x32" />
-      </Head>
       <body className={`bg-[#1B2432] ${inter.className}`}>
         <div className="flex min-h-screen flex-col items-center justify-center gap-y-32 p-8 md:px-16 lg:px-32 xl:px-64">
           <Navigation />
