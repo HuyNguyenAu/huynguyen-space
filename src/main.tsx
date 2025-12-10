47c546e3d65ae1898bd87270410ca5e19ea2ced3
HuyNguyenAu <github@huynguyen.email>
2025-12-10 22:47:07 +1100

    WIP

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HomePage } from "@/pages/home";
 import { BrowserRouter, Route, Routes } from "react-router";
-import { ArticlePage } from "./pages/article";
+import { ExperienceJetchargeFullStackEngineerPage } from "./pages/experience-jetcharge-full-stack-engineer";
 
 const rootElement = document.getElementById("root");
 
@@ -20,32 +20,7 @@ createRoot(rootElement).render(
         <Route path="experience">
           <Route
             path="jetcharge/full-stack-software-engineer"
-            element={
-              <ArticlePage>
-                <h3>Full Stack Software Engineer</h3>
-                <h4>Company: JET Charge</h4>
-                <h4>Duration: Oct 2021 - Present</h4>
-                <h1></h1>
-                <h4>CORE - EV Energy Management System</h4>
-                <p>
-                  On-premise and autonomous Electric Vehicle (EV) charging load
-                  management system (CORE).
-                </p>
-                <p>
-                  Developed Australia's first world leading UI for deploying and managing
-                  on-premise EV charging load management systems.
-                </p>
-                <h4>Illuminate - EV Charging Management Platform</h4>
-                <p>
-                  Cloud-based platform for Electric Vehicle (EV) smart-charging
-                  asset management (Illuminate).
-                </p>
-                <p>
-                  Fixed various bugs and issues in the existing codebase,
-                  ensuring the platform's stability and reliability.
-                </p>
-              </ArticlePage>
-            }
+            element={<ExperienceJetchargeFullStackEngineerPage />}
           />
         </Route>
       </Routes>
---
+++ src/pages/experience-jetcharge-full-stack-engineer.tsx
@@ -0,0 +1,85 @@
+import { Article } from "@/components/article";
+
+export const ExperienceJetchargeFullStackEngineerPage = () => {
+  return (
+    <Article>
+      <h3>Full Stack Software Engineer</h3>
+      <p>
+        Company: JET Charge<br></br>Duration: Oct 2021 - Present
+      </p>
+      <h4>EV Energy Management System (CORE)</h4>
+      <p>
+        An on-premise Electric Vehicle (EV) charging load management system
+        (CORE).
+      </p>
+      <ul>
+        <li>
+          Developed Australia's first leading web app using node based UI to
+          setup, deploy, and manage an EV charging site.
+          <ul>
+            <li>
+              Built UI to create and manage devices such as chargers, power
+              meters, and distribution boards, and configure load management
+              strategies.
+            </li>
+            <li>
+              Implemented real-time visualisation to monitor charging station
+              performance and energy consumption.
+            </li>
+          </ul>
+        </li>
+        <li>
+          Designed and developed RESTful APIs for communication between the
+          local front-end and back-end systems.
+          <ul>
+            <li>
+              Created endpoints for authentication, managing user accounts,
+              devices, and load management configurations.
+            </li>
+            <li>Real-time data streaming to front-end using SignalR.</li>
+          </ul>
+        </li>
+      </ul>
+      <h4>Illuminate - EV Charging Management Platform</h4>
+      <p>
+        Cloud-based platform for Electric Vehicle (EV) smart-charging asset
+        management (Illuminate).
+      </p>
+      <ul>
+        <li>
+          Developed apartment EV charging billing solution.
+          <ul>
+            <li>
+              Built UI assign chargers to residents and set tarriffs for EV
+              charging sessions.
+            </li>
+            <li>
+              Intergated with Stripe to handle payment processing and invoicing
+              for EV charging sessions.
+            </li>
+            <li>
+              Implemented RESTful API endpoints to manage residents, chargers,
+              and billing.
+            </li>
+          </ul>
+        </li>
+        <li>
+          Developed partner and contractor management solution enabling
+          organisations to provision and manage EV charging assets.
+          <ul>
+            <li>
+              Built UI and workflows for partners and contractors to manage
+              their clients' EV charging assets.
+            </li>
+            <li>
+              Ideate and implemented a tailored solution to allow partners and
+              contractors to view and manage multiple organisations' EV charging
+              assets from a single account while maintaining full compliance and
+              compatibility with existing codebase.
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </Article>
+  );
+};
