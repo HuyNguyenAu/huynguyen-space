5e0c0b58e842990558666049d1684b755be0dae8
HuyNguyenAu <18211652+HuyNguyenAu@users.noreply.github.com>
2026-06-06 22:49:44 +1000

    Update Skills section to include Bicep and refine platforms; remove Entity Framework Core and unnecessary platforms

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -9,20 +9,20 @@ export const Skills = () => {
         <Body className="m-0 mt-2">C#</Body>
         <Body className="m-0 mt-2">Typescript</Body>
         <Body className="m-0 mt-2">SQL</Body>
+        <Body className="m-0 mt-2">Bicep</Body>
       </div>
 
       <div>
         <Heading className="md:text-center">FRAMEWORKS</Heading>
         <Body className="md:text-center m-0 mt-2">.NET</Body>
         <Body className="md:text-center m-0 mt-2">React</Body>
-        <Body className="md:text-center m-0 mt-2">Entity Framework Core</Body>
       </div>
 
       <div>
         <Heading className="md:text-end">PLATFORMS</Heading>
-        <Body className="md:text-end m-0 mt-2">Windows</Body>
-        <Body className="md:text-end m-0 mt-2">Linux</Body>
-        <Body className="md:text-end m-0 mt-2">MacOS</Body>
+        <Body className="md:text-end m-0 mt-2">Azure</Body>
+        <Body className="md:text-end m-0 mt-2">GitHub Actions</Body>
+        <Body className="md:text-end m-0 mt-2">Azure DevOps</Body>
       </div>
     </Section>
   );
